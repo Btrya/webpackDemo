f61b6b9005b9ece5bd15da56103a25cd297280e6
Btrya <584683599@qq,com>
2020-06-07 16:29:55 +0800

    commit first

---
+++ src/entry.js
@@ -0,0 +1,7 @@
+import css from './css/style.css';
+import less from './css/black.less';
+import sass from './css/nav.scss';
+{
+  const caibinString = 'Hello World!!!'
+  document.getElementById('title').innerHTML = caibinString
+}
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path')
+const glob = require('glob')
+const UglifyPlugin = require('uglifyjs-webpack-plugin')
+const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const PurifyCSSPlugin = require("purifycss-webpack")
+
+let website = {
+    publicPath: "http://localhost:9527/"
+}
+
+module.exports = {
+    entry: {
+        bundle: './src/entry.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        publicPath: website.publicPath
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // use: [
+                //     'style-loader',
+                //     { loader: 'css-loader', options: { importLoaders: 1 } },
+                //     'postcss-loader'
+                // ]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader",
+                        options: {importLoaders: 1}
+                    },
+                    "postcss-loader"]
+                })
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 5000,
+                            outputPath: 'images/',
+                            esModule: false
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(htm|html)$/i,
+                loader: ['html-withimg-loader']
+            },
+            {
+                test: /\.less$/,
+                // use: [{
+                //     loader: "style-loader"
+                // },{
+                //     loader: "css-loader"
+                // },{
+                //     loader: "less-loader"
+                // }]
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader"
+                    },{
+                        loader: "less-loader"
+                    }]
+                })
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader"
+                    },{
+                        loader: "sass-loader"
+                    }]
+                })
+            },
+            {
+                test: /\.(jsx|js)$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        // new UglifyPlugin(),
+        new HtmlPlugin({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin("css/style.css"),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/*.html'))
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: 'localhost',
+        compress: true,
+        port: 9527
+    }
+}
